79e161a6326cdd1e79325e99c70561979b256a0b
Yi-Chun, Yen <ghostskou@gmail.com>
2016-11-09 08:29:21 +0800

    Follow initial slides

--- app/src/main/java/project/yen/yichun/todoapp/MainActivity.java
+++ app/src/main/java/project/yen/yichun/todoapp/MainActivity.java
@@ -2,22 +2,81 @@ package project.yen.yichun.todoapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readItems();
+        lvItems = (ListView) findViewById(R.id.activity_main_list_view);
+        editText = (EditText) findViewById(R.id.activity_main_et);
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+        initListener();
     }
+
+    private void initListener() {
+        lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+
 
     /**
      * Click the add item button to add the new task to local.
      *
      * @param view
      */
-    public void AddTask(View view) {
+    public void onAddItem(View view) {
+        itemsAdapter.add(editText.getText().toString());
+        editText.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File fileDir = getFilesDir();
+        File todoFile = new File(fileDir, "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        }
     }
 }
